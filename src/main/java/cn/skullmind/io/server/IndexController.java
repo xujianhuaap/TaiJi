9411dfe6d167055e85cd18582eeb864a3682e1cf
skull <xujianhuaap@gmail.com>
2019-04-21 23:29:00 +0800

    use template

--- src/main/java/cn/skullmind/io/server/IndexController.java
+++ src/main/java/cn/skullmind/io/server/IndexController.java
@@ -1,13 +1,18 @@
 package cn.skullmind.io.server;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class IndexController {
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public String hello(){
-        return "hello world";
+    public ModelAndView hello(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        modelAndView.addObject("title","spring boot");
+        return modelAndView;
     }
 }
