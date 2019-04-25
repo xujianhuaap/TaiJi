f4c39fa2da6b584156821f190c07f11727fe3733
skull <xujianhuaap@gmail.com>
2019-04-25 23:26:57 +0800

    add conig

--- src/main/java/cn/skullmind/io/server/index/IndexController.java
+++ src/main/java/cn/skullmind/io/server/index/IndexController.java
@@ -1,9 +1,8 @@
-package cn.skullmind.io.server;
+package cn.skullmind.io.server.index;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
