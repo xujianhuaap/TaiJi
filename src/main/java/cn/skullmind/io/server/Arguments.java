478932be4ec471f04f42c443f705cb6eb14f34bd
skull <xujianhuaap@gmail.com>
2019-04-27 00:00:38 +0800

    application arguments

--- src/main/java/cn/skullmind/io/server/Arguments.java
+++ src/main/java/cn/skullmind/io/server/Arguments.java
@@ -6,14 +6,18 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+/***
+ * java -jar server.jar version_code=10 version_name=magic
+ */
 @Component
 public class Arguments {
     @Autowired
     public Arguments(ApplicationArguments arguments) {
         boolean debug = arguments.containsOption("debug");
         List<String> strings = arguments.getNonOptionArgs();
+        System.out.println("=====arguments=====");
         for(String s:strings){
-            System.out.println(s);
+            System.out.println("arguments===>"+s);
         }
 
     }
--- src/main/java/cn/skullmind/io/server/index/IndexController.java
+++ src/main/java/cn/skullmind/io/server/index/IndexController.java
@@ -1,5 +1,7 @@
 package cn.skullmind.io.server.index;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,11 +9,14 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
+    @Autowired
+    private ApplicationArguments applicationArguments;
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public ModelAndView hello(){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("index");
         modelAndView.addObject("title","spring boot");
+        modelAndView.addObject("arguments",applicationArguments.getNonOptionArgs());
         return modelAndView;
     }
 }
